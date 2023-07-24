749e4b90a35e577fca3181166cfd734d36f3a448
Ertowin <bimbosisaac@gmail.com>
2023-07-24 11:00:11 +0100

    Positive anything is better than negative nothing

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,6 @@
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+
+
 print("First 3 letters:",word[0:3])
 print("Last 2 letters:", word[7:9])
-print("Middle word:", word[1:8])
+print("Middle word:", word[1:8])   
